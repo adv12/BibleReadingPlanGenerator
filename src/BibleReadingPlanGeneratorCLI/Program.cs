da324177c891ebfac5e2d0bece59b3b0fbc5a83d
Andrew Vardeman <andrewdev@the-vardemans.net>
2019-12-08 21:15:02 -0600

    Established projects, created a class for  holding counts, and tested deserializing a JSON list of count spec things

---
+++ src/BibleReadingPlanGeneratorCLI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using BibleReadingPlanGeneratorLib;
+
+namespace BibleReadingPlanGeneratorCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string countsJson = File.ReadAllText(args[0]);
+            BibleCountsSpec[] countsSpec =
+                JsonSerializer.Deserialize<BibleCountsSpec[]>(countsJson);
+        }
+    }
+}
---
+++ src/BibleReadingPlanGeneratorLib/BibleCountsSpec.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BibleReadingPlanGeneratorLib
+{
+    public class BibleCountsSpec
+    {
+        public string Abbreviation { get; set; }
+
+        public string Name { get; set; }
+
+        public List<List<int>> CharacterCounts { get; set; }
+
+        public List<List<int>> WordCounts { get; set; }
+    }
+}
